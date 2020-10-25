1052c33673e6203345db706befaf6962f245b536
Refaey <abdullah.m.alrefaey@gmail.com>
2020-10-25 19:02:00 +0200

    Added Firebase Library

--- ESP/esp.ino
+++ ESP/esp.ino
@@ -1,3 +1,10 @@
+#include <ESP8266WiFi.h>
+#include "lib/FirebaseESP8266.h"
+#include "lib/FirebaseESP8266HTTPClient.h"
+#include "lib/FirebaseFS.h"
+#include "lib/FirebaseJson.h"
+
+
 void setup()
 {
   // put your setup code here, to run once:
