0009889d5f6665a9161bbd1d8d3c28e67ebdf660
Ahmad <ahmedmageed19@gmail.com>
2020-10-26 20:01:30 +0200

    added get firebase data

--- VitaMon/client/main.js
+++ VitaMon/client/main.js
@@ -1,19 +1,33 @@
 import { Template } from 'meteor/templating';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './main.html';
+// Load Library
+const firebase = require('firebase-admin');
+
+// get service key file
+const servicekey = require('../esp-1st-project-firebase-adminsdk-atckp-7d0a35b5d7.json');
+
+// Init App
 
 function rand(){
   return Math.random();
 }
 Template.body.events({
   'click button'(event, instance) {
-    let interval = setInterval(function() {
-
+    firebase.initializeApp({
+      credential: firebase.credential.cert(servicekey),
+      databaseURL: 'https://esp-1st-project.firebaseio.com'
+    });
+  
+  
+  let datapoint = firebase.database().ref();
+  let data = datapoint.child("Reading");
+  data.on("value", (snap)=>{
       let time = new Date();
 
       let update = {
         x:  [[time]],
-        y: [[rand()]]
+        y: [[snap.val()]]
       }
 
       let olderTime = time.setMinutes(time.getMinutes() - 1);
@@ -28,7 +42,6 @@ Template.body.events({
 
       Plotly.relayout('graph', minuteView);
       Plotly.extendTraces('graph', update, [0])
-
-    }, 50);
+  });
   },
 });
