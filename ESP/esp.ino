2526685853cd0fe5b5d834c2a98e0c5b34b06767
Refaey <abdullah.m.alrefaey@gmail.com>
2020-10-26 06:03:48 +0200

    fix esp.ino

--- ESP/esp.ino
+++ ESP/esp.ino
@@ -1,29 +1,26 @@
-#include "lib/FirebaseESP8266.h"
 #include <ESP8266WiFi.h>
-
+#include <FirebaseESP8266.h>
 #include <FirebaseESP8266HTTPClient.h>
 #include <FirebaseFS.h>
 #include <FirebaseJson.h>
-//using namespace FirebaseESP8266;
 
-// Set these to run example.
+// Configure Firebase Variables
 #define FIREBASE_HOST "esp-1st-project.firebaseio.com"
 #define FIREBASE_AUTH "LJTM2sDQv2ehHguRXcXtwtPBMdGCDJTJ50FTBMVz"
 #define WIFI_SSID "Cold"
 #define WIFI_PASSWORD "salah199998"
 
-
 // Declare the Firebase Data object in the global scope
 FirebaseData firebaseData;
 
 String myString;
-int vr = A0; // variable resistor connected
-int sdata = 0; // The variable resistor value will be stored in sdata.
+int vr = A0;         // variable resistor connected
+int sdata = 0;       // The variable resistor value will be stored in sdata.
 
 void setup()
 {
     // Debug console
-    Serial.begin(9600);
+    Serial.begin(115200);
     pinMode(vr , INPUT);
     pinMode(D0, OUTPUT);
   
@@ -33,18 +30,16 @@ void setup()
   
     while (WiFi.status() != WL_CONNECTED)
     {
-      Serial.print(".");
-      delay(500);
+        Serial.print(".");
+        delay(500);
     }
     Serial.println();
     Serial.print("connected: ");
     Serial.println(WiFi.localIP());
     
     Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
-    // Firebase.push("Variable/Value", "fahad");
-    
-    
-    //4. Enable auto reconnect the WiFi when connection lost
+        
+    // Enable auto reconnect the WiFi when connection lost
     Firebase.reconnectWiFi(true);
 }
 
@@ -52,22 +47,17 @@ void setup()
 void loop()
 
 {
-  sdata = analogRead(vr);
-//  myString = String(sdata);
-  Serial.println(sdata);
-  if (Firebase.setFloat(firebaseData, "/Reading", sdata))
-  {
-     //Success
-     Serial.println("Set int data success");
-  }
-  else
-  {
-    //Failed?, get the error reason from firebaseData
-
-    Serial.print("Error in setInt, ");
-    Serial.println(firebaseData.errorReason());
-  }
+    sdata = analogRead(vr);
+    Serial.println(sdata);
+    
+    if (Firebase.setFloat(firebaseData, "/Reading", sdata)) {
+        // Success
+        Serial.println("Set int data success");
+    } else {
+        // Failed?, get the error reason from firebaseDate
+        Serial.print("Error in setInt, ");
+        Serial.println(firebaseData.errorReason());
+    }
   
-    // Firebase.pushString("/Reading", myString);
-  delay(200);
+    delay(200);
 }
